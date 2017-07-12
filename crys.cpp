3485e2a8f2a84e2c8f78fe162e6d39d6caa37f63
Annya88888 <lenel88@yandex.ru>
2017-07-12 11:51:52 +0300

    Please

---
+++ crys.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iostream>
+
+
+void cry ()
+{
+	int size = 10;
+	std::cout <<"A";
+	for (int i = 0; i < size; i ++)
+	{
+		std::cout<<"a";
+	}
+	std::cout<<std::endl;
+	return:
+}
---
+++ mains.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "help.h"
+
+/*!
+	\date 12/7/2017
+*/
+
+int main()
+{
+	std::cout << "Today I want to tell you about terruble thing..." << std::endl;
+	void tell();
+	void cry();
+	return 0;
+}
